9532ad176d3e388742e0771eeb4f24e33d3cc23c
So Muzaffari <so.muzaff@gmail.com>
2026-01-01 08:19:26 +0330

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
-    id("com.android.application") version "8.5.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
-    id("com.google.devtools.ksp") version "1.9.24-1.0.20" apply false
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    // REMOVED KSP - using kapt instead
 }
 
 tasks.register("clean", Delete::class) {
-    delete(layout.buildDirectory)
+    delete(rootProject.buildDir)
 }
