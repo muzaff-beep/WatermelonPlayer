2c59c09b3c9d285519d59431c550f0b952ef7696
So Muzaffari <so.muzaff@gmail.com>
2026-01-01 20:40:05 +0330

    Create EditionAwareBilling.kt

---
+++ app/src/main/java/com/watermelon/player/billing/EditionAwareBilling.kt
@@ -0,0 +1,32 @@
+package com.watermelon.player.billing
+
+import android.content.Context
+import com.watermelon.player.config.EditionManager
+import com.watermelon.player.billing.IranianPaymentProcessor
+import com.watermelon.player.billing.GooglePlayBilling
+
+class EditionAwareBilling(private val context: Context) {
+    private val iranianProcessor = IranianPaymentProcessor()
+    private val googlePlayBilling = GooglePlayBilling(context)
+
+    fun startPurchase(onSuccess: () -> Unit, onFail: (String) -> Unit) {
+        if (EditionManager.isIranEdition()) {
+            iranianProcessor.startIranianPurchase(context, onSuccess, onFail)
+        } else {
+            googlePlayBilling.startGlobalPurchase(onSuccess, onFail)
+        }
+    }
+
+    fun verifyPurchase(): Boolean {
+        return if (EditionManager.isIranEdition()) {
+            iranianProcessor.verifyIranianPurchase()
+        } else {
+            googlePlayBilling.verifyGlobalPurchase()
+        }
+    }
+
+    fun unlockFullVersion() {
+        // Shared unlock logic
+        // Set shared preference "unlocked = true"
+    }
+}
