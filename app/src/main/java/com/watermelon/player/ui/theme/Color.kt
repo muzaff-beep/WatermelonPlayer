6c6af7ca2bad758b9c109406142e622240409dcc
So Muzaffari <so.muzaff@gmail.com>
2026-01-02 12:17:21 +0330

    Update Color.kt

--- app/src/main/java/com/watermelon/player/ui/theme/Color.kt
+++ app/src/main/java/com/watermelon/player/ui/theme/Color.kt
@@ -2,7 +2,14 @@ package com.watermelon.player.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val WatermelonRed = Color(0xFFF44336)
-val WatermelonGreen = Color(0xFF4CAF50)
-val WatermelonDarkGreen = Color(0xFF388E3C)
-val WatermelonLightGreen = Color(0xFF81C784)
+val WatermelonRedLight = Color(0xFFEF5350)
+val WatermelonGreenLight = Color(0xFF66BB6A)
+val WatermelonAccentLight = Color(0xFFFF4081)
+
+val WatermelonRedDark = Color(0xFFE53935)
+val WatermelonGreenDark = Color(0xFF43A047)
+val WatermelonAccentDark = Color(0xFFF50057)
+
+val PureBlack = Color(0xFF000000)
+val SurfaceDark = Color(0xFF121212)
+val SurfaceLight = Color(0xFFF5F5F5)
