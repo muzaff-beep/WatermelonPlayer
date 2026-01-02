b207f05ea5af47f69e4d070a533eb647545e26d6
So Muzaffari <so.muzaff@gmail.com>
2026-01-02 12:58:01 +0330

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
-    // REMOVED KSP - using kapt instead
+    id("com.android.application") version "8.5.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    id("com.google.dagger.hilt.android") version "2.51" apply false
+    id("org.jetbrains.kotlin.kapt") version "1.9.24" apply false
 }
 
-tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
-}
+true // Needed to make the DSL work correctly in Kotlin build scripts
