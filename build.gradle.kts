97627c6c055f7a242f8797587a852aba2b7f5b2a
So Muzaffari <so.muzaff@gmail.com>
2026-01-04 00:23:39 +0330

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,12 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+// Top-level build file
 plugins {
-    id("com.android.application") version "8.5.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
-    id("com.google.devtools.ksp") version "1.9.24-1.0.20" apply false
-    id("org.jetbrains.kotlin.plugin.parcelize") version "1.9.24" apply false  // FIXED: full ID
+    id("com.android.application") version "8.5.0" apply false // Update to your AGP version
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false // Or latest Kotlin
+    id("com.google.dagger.hilt.android") version "2.51" apply false
+    id("com.google.devtools.ksp") version "1.9.24-1.0.20" apply false // For Room KSP
 }
 
-tasks.register("clean", Delete::class) {
-    delete(layout.buildDirectory)
+repositories {
+    google()
+    mavenCentral()
 }
