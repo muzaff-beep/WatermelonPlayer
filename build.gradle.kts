283238a38301695ba4fc6c7e24445835295afbcc
So Muzaffari <so.muzaff@gmail.com>
2026-01-03 17:51:28 +0330

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,11 @@
+// Top-level build file - common config for all modules
 plugins {
-    id("com.android.application") version "8.13.0" apply false
-    id("com.android.library") version "8.13.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.3.0" apply false
-    id("org.jetbrains.kotlin.plugin.parcelize") version "2.3.0" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.3.0" apply false
+    id("com.android.application") version "8.5.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    id("com.google.devtools.ksp") version "1.9.24-1.0.20" apply false
+    id("kotlin-parcelize") apply false
+}
+
+tasks.register("clean", Delete::class) {
+    delete(layout.buildDirectory)
 }
