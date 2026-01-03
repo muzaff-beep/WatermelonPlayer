975437e28a776b3914f9de3d0594cc028ec5807a
So Muzaffari <so.muzaff@gmail.com>
2026-01-03 11:56:51 +0330

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
+// WatermelonPlayer/build.gradle.kts  (root project)
 plugins {
     id("com.android.application") version "8.7.0" apply false
     id("com.android.library") version "8.7.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.20" apply false  // Critical: update Kotlin to 2.0+
-    // Add other plugins here if needed (e.g., hilt, etc.)
+    id("org.jetbrains.kotlin.android") version "2.0.20" apply false
+    id("kotlin-parcelize") version "2.0.20" apply false
 }
