eca5dd1e927c49c9213176c6edfd6c0f37485cd8
So Muzaffari <so.muzaff@gmail.com>
2026-01-03 12:26:15 +0330

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
-// WatermelonPlayer/build.gradle.kts (root project)
 plugins {
     id("com.android.application") version "8.13.0" apply false
     id("com.android.library") version "8.13.0" apply false
     id("org.jetbrains.kotlin.android") version "2.3.0" apply false
     id("org.jetbrains.kotlin.plugin.parcelize") version "2.3.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.3.0" apply false  // ← ADD THIS LINE
 }
