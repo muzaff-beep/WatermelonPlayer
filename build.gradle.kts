ad609fe7250caaec65b9cbd636ba74cfe2fe8aa6
So Muzaffari <so.muzaff@gmail.com>
2026-01-04 02:41:26 +0330

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,10 @@
 // Top-level build file
 plugins {
-    id("com.android.application") version "8.5.0" apply false // Update to your AGP version
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false // Or latest Kotlin
+    id("com.android.application") version "8.7.0" apply false  // Latest stable AGP ~late 2025/early 2026
+    id("org.jetbrains.kotlin.android") version "2.0.20" apply false  // Latest stable Kotlin
     id("com.google.dagger.hilt.android") version "2.51" apply false
-    id("com.google.devtools.ksp") version "1.9.24-1.0.20" apply false // For Room KSP
+    id("com.google.devtools.ksp") version "2.0.20-1.0.25" apply false  // Matches Kotlin for KSP/Room
 }
 
-repositories {
-    google()
-    mavenCentral()
-}
+// No repositories block here anymore!
+// All repositories are now centralized in settings.gradle.kts
