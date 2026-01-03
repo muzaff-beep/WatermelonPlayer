fdce55d311bbe545c32ac397c2a21663f1971436
So Muzaffari <so.muzaff@gmail.com>
2026-01-03 18:14:03 +0330

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("com.android.application") version "8.5.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
     id("com.google.devtools.ksp") version "1.9.24-1.0.20" apply false
-    id("kotlin-parcelize") version "1.9.24" apply false  // FIXED: Added version
+    id("org.jetbrains.kotlin.plugin.parcelize") version "1.9.24" apply false  // FIXED: Correct ID
 }
 
 tasks.register("clean", Delete::class) {
