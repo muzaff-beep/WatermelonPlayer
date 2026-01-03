61bb8dbe8ba5935e350a5a2ab2a1778fa94147fc
So Muzaffari <so.muzaff@gmail.com>
2026-01-04 02:50:00 +0330

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file
 plugins {
-    id("com.android.application") version "8.7.0" apply false  // Latest stable AGP early 2026
-    id("org.jetbrains.kotlin.android") version "2.3.0" apply false  // Latest stable Kotlin (Dec 2025 release)
+    id("com.android.application") version "8.7.0" apply false  // Compatible stable AGP
+    id("org.jetbrains.kotlin.android") version "2.3.0" apply false  // Latest stable Kotlin (Dec 2025)
     id("com.google.dagger.hilt.android") version "2.51" apply false
-    id("com.google.devtools.ksp") version "2.3.0-1.0.25" apply false  // Matches Kotlin
+    id("com.google.devtools.ksp") version "2.3.0" apply false  // Fixed: Latest stable KSP
 }
